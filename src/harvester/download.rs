2f9d3308c0568553cfc9b7e98df2a526ad8f3ace
Mark Cooper <551470+mark-cooper@users.noreply.github.com>
2025-12-21 17:01:48 -0800

    Init step for download

--- src/harvester/download.rs
+++ src/harvester/download.rs
@@ -1,5 +1,24 @@
+use crate::harvester::oai::OaiRecordStatus;
+
 use super::Harvester;
 
 pub(super) async fn run(harvester: &Harvester) -> anyhow::Result<()> {
     todo!()
+}
+
+async fn fetch_pending_records(harvester: &Harvester) -> anyhow::Result<Vec<OaiRecordStatus>> {
+    let records: Vec<OaiRecordStatus> = sqlx::query_as!(
+        OaiRecordStatus,
+        r#"
+        SELECT endpoint, metadata_prefix, identifier, status
+        FROM oai_records
+        WHERE endpoint = $1 AND metadata_prefix = $2 AND status = 'pending'
+        "#,
+        &harvester.config.endpoint,
+        &harvester.config.metadata_prefix
+    )
+    .fetch_all(&harvester.pool)
+    .await?;
+
+    Ok(records)
 }
