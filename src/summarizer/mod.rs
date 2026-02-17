ce9f668e11b8fd846fdc0aaca1ef9a7dc39c2ae8
Mark Cooper <551470+mark-cooper@users.noreply.github.com>
2026-02-17 15:06:15 -0800

    Summarizer cont.

--- src/summarizer/mod.rs
+++ src/summarizer/mod.rs
@@ -1,6 +1,9 @@
 #![allow(dead_code)] // While WIP
 
+use std::path::PathBuf;
+
 use clap::Args;
+use sqlx::PgPool;
 
 mod gemini;
 
@@ -13,12 +16,54 @@ pub struct SummarizerArgs {
     #[arg(short, long)]
     pub metadata_prefix: String,
 
-    // OAI identifier
+    /// OAI identifier
     #[arg(short, long)]
     pub identifier: Option<String>,
+
+    /// Base directory for downloads
+    #[arg(short, long, default_value = "data")]
+    pub dir: PathBuf,
+
+    /// Max number of concurrent summarizer requests
+    #[arg(short, long, default_value_t = 1)]
+    pub concurrency: u8,
+
+    /// Per-record timeout for summarizer api calls
+    #[arg(short, long, default_value_t = 60)]
+    pub timeout_seconds: u64,
 }
 
-pub struct SummarizerContext {}
+pub struct SummarizerContext {
+    pool: PgPool,
+    endpoint: String,
+    metadata_prefix: String,
+    identifier: Option<String>,
+    run_options: SummarizerRunOptions,
+}
+
+impl SummarizerContext {
+    pub fn new(
+        pool: PgPool,
+        endpoint: String,
+        metadata_prefix: String,
+        identifier: Option<String>,
+        run_options: SummarizerRunOptions,
+    ) -> Self {
+        Self {
+            pool,
+            endpoint,
+            metadata_prefix,
+            identifier,
+            run_options,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct SummarizerRunOptions {
+    pub(crate) concurrency: u8,
+    pub(crate) timeout_seconds: u64,
+}
 
 pub trait Summarizer {
     fn summarize(&self, content: String) -> anyhow::Result<()>;
